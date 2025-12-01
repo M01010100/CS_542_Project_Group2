53c6d5fa1bc32e998995f9036f5b85ebd36de026
M01010100 <townsemc@sunypoly.edu>
2025-12-01 18:19:09 -0500

    website

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import Apriori as apriori_module
 import TimeSeries as ts_module   
 import Clustering as clust_module
 def main():
-    #apriori_module.run_apriori()
-    #apriori_module.interactive_predict_for_customer()
-    #ts_module.time_series_forecast()
+    apriori_module.run_apriori()
+    ts_module.time_series_forecast()
     clust_module.customer_segmentation()
 
 
